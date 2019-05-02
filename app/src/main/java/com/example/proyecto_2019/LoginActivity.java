299b0fcb582005b2bb5b0b757c703fb8320c5ca4
Ronal <ronal12.rhr@gmail.com>
2019-05-02 18:02:10 -0500

    commint 2

--- app/src/main/java/com/example/proyecto_2019/LoginActivity.java
+++ app/src/main/java/com/example/proyecto_2019/LoginActivity.java
@@ -1,16 +1,27 @@
 package com.example.proyecto_2019;
 
+
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+
 
 public class LoginActivity extends AppCompatActivity {
 
+    EditText et_user,et_password;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //SplashScream
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        //casting
+        et_user = findViewById(R.id.et_usuario);
+        et_password = findViewById(R.id.et_contrasena);
+
     }
 
     public void nuevoRegistro(View view){
@@ -18,8 +29,39 @@ public class LoginActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void main(View view){
-       Intent intent = new Intent(this,ContainerActivity.class);
+    public void login(View view){
+
+        Intent intent = new Intent(this,ContainerActivity.class);
         startActivity(intent);
+
+     /*  SqLiteDB sqLiteDB = new SqLiteDB(this,"db",null,1);
+       SQLiteDatabase database = sqLiteDB.getWritableDatabase();
+
+       String user = et_user.getText().toString();
+       String pass = et_password.getText().toString();
+
+
+       if(!user.isEmpty() && !pass.isEmpty()){
+
+           Cursor cursor = database.rawQuery("select user,contrasena from usuario where user='"+user+"'and contrasena='"+pass +"'" ,null);
+            if (cursor.moveToFirst()){
+                String u = cursor.getString(0);
+                String p = cursor.getString(1);
+
+                if (user.equals(u) && pass.equals(p)){
+                    Intent intent = new Intent(this,ContainerActivity.class);
+                    startActivity(intent);
+                    Toast.makeText(this,"Bienvenido",Toast.LENGTH_SHORT).show();
+                    et_user.setText("");
+                    et_password.setText("");
+                }
+
+            }else {
+                Toast.makeText(this,"El usuario no existe",Toast.LENGTH_SHORT).show();
+            }
+       }else {
+           Toast.makeText(this,"Campos vacios",Toast.LENGTH_SHORT).show();
+       }*/
     }
+
 }
--- app/src/main/java/com/example/proyecto_2019/RegistroActivity.java
+++ app/src/main/java/com/example/proyecto_2019/RegistroActivity.java
@@ -1,10 +1,16 @@
 package com.example.proyecto_2019;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+
 
 public class RegistroActivity extends AppCompatActivity {
+
+    EditText et_user,et_email,et_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +18,10 @@ public class RegistroActivity extends AppCompatActivity {
         setContentView(R.layout.activity_registro);
 
         showToolbar(getResources().getString(R.string.crear),true);
+        et_user = findViewById(R.id.crearUsuario);
+        et_email = findViewById(R.id.email);
+        et_password = findViewById(R.id.crearContrasena);
+
 
     }
 
@@ -20,6 +30,31 @@ public class RegistroActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle(titulo);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void registro(View view){
+
+        /*SqLiteDB sqLiteDB= new SqLiteDB(this,"db",null,1);
+        SQLiteDatabase database = sqLiteDB.getWritableDatabase();
+        String uid = et_user.getText().toString();
+        String mail = et_email.getText().toString();
+        String pwd = et_password.getText().toString();
+
+        if(!uid.isEmpty() && !pwd.isEmpty() && !mail.isEmpty()){
+            ContentValues values = new ContentValues();
+            values.put("user",uid);
+            values.put("email",mail);
+            values.put("password",pwd);
+            database.insert("usuario",null,values);
+            database.close();
+            et_user.setText("");
+            et_email.setText("");
+            et_password.setText("");
+            Toast.makeText(this,"Registro Exitoso",Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(this,"Error",Toast.LENGTH_SHORT).show();
+        }*/
+
     }
 
 }
